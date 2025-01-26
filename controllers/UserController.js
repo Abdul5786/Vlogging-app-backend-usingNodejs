9c7391a824886f2a687e55effdbff1eac826232d
Abdul Khaliq <ak882710@gmail.com>
2025-01-27 00:56:04 +0530

    added update and delete api

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -47,5 +47,68 @@ export const getAllUserList= async(req,res)=>{
        return res.status(500).json({error:"internal servor error"})
        
       }
-
+    
 }
+
+
+export const updateUser= async(req,res)=>{
+                   
+      try {
+
+
+          const id= req.params.id;
+
+                  // Check if the ID is provided
+    if (!id) {
+      return res.status(400).json({ message: "User ID is required" });
+    }
+
+
+                const userExist = await  User.findOne({_id:id});
+                if(!userExist)
+                {
+                      return res.status(400).json({message:"user not found with this id"})
+                }
+
+               const updateUser =   await  User.findByIdAndUpdate(id,req.body,{new: true})
+               
+                return res.status(200).json(updateUser)
+          
+      } catch (error) {
+          
+          return res.status(500).json({error:"internal servor error"})
+      }
+                 
+         
+}
+
+
+ export const deleteUserById= async (req,res)=>{
+
+                              try {
+
+                                    const id= req.params.id;
+
+                                    // Check if the ID is provided
+                                         if (!id) {
+                                          return res.status(400).json({ message: "User ID is required" });
+                                      }
+
+
+                                    const userExist = await  User.findOne({_id:id});
+
+                                     if(!userExist)
+                                     {
+                                          return res.status(400).json({message:"user not exist by this id"})
+                                     }
+
+                                          await User.deleteUserById(id);
+                                          res.status(200).json({message:"user deleted succesfully"})
+                                            
+                                    
+                              } catch (error) {
+
+                                    return res.status(500).json({error:"internal servor error"})
+                                    
+                              }
+ }
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,10 +1,13 @@
 import express from "express"
-import {createUser,getAllUserList} from "../controllers/UserController.js"
+import {createUser,getAllUserList,updateUser,deleteUserById} from "../controllers/UserController.js"
 
  const routes = express.Router();
 
  routes.post("/create",createUser)
  routes.get("/all",getAllUserList)
+ routes.put("/update/:id",updateUser)
+ routes.delete("/delete/:id",deleteUserById)
+
 
 
 
